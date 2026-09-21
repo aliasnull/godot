139d7191925a00112ef8597f967a96453096d12f
AliasNull <100416456+aliasnull@users.noreply.github.com>
2026-09-21 23:16:49 +0530

    Fix native MCP service notification

--- platform/android/java/editor/src/main/java/org/godotengine/editor/BaseGodotEditor.kt
+++ platform/android/java/editor/src/main/java/org/godotengine/editor/BaseGodotEditor.kt
@@ -339,10 +339,6 @@ abstract class BaseGodotEditor : GodotActivity(), GameMenuFragment.GameMenuListe
 		}
 	}
 
-	startService(
-            Intent(this, McpBridgeService::class.java)
-    )
-
 	override fun onConfigurationChanged(newConfig: Configuration) {
 		super.onConfigurationChanged(newConfig)
 
